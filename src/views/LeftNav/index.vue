89b7bac98dc2920edc7bf95702762979e8b5eb3d
Hyman <hyman4hu@qq.com>
2022-06-10 00:18:59 +0800

    ttt

--- src/views/LeftNav/index.vue
+++ src/views/LeftNav/index.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <div class="box">
-            123
+            <div class="avatar-div">
+                <img class="avatar" src="chat_white.svg" alt="头像" title="头像">
+            </div>
         </div>
     </div>
 </template>
@@ -15,5 +17,21 @@ export default {
     height: 100%;
     width: 100%;
     background-color: rgb(46, 46, 46);
+
+    .avatar-div {
+        text-align: center;
+        padding-top: 16px;
+
+        .avatar {
+            height: 40px;
+            cursor: pointer;
+            transition: all 0.3s;
+
+            &:hover {
+                transform: scale(1.1);
+            }
+        }
+    }
+
 }
 </style>
